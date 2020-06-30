152ba8d03a9028f093bd6787abfcb98052fd542a
Erik <erik.benton@hotmail.com>
2020-06-30 12:11:17 -0700

    Creating Control Fragment layout and fixing some issues with the ActionBar

--- app/src/main/java/com/example/m4359touchscreen/Activities/MainActivity.java
+++ app/src/main/java/com/example/m4359touchscreen/Activities/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.m4359touchscreen.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.m4359touchscreen.Fragments.ControlsConfigFragment;
+import com.example.m4359touchscreen.Fragments.ControlsFragment;
 import com.example.m4359touchscreen.Fragments.ExtensometerReadingsFragment;
 import com.example.m4359touchscreen.Fragments.LoadReadingsFragment;
 import com.example.m4359touchscreen.Fragments.OptionsConfigFragment;
@@ -16,66 +24,70 @@ import com.example.m4359touchscreen.Fragments.TemperatureReadingFragment;
 import com.example.m4359touchscreen.Fragments.TimerReadingsFragment;
 import com.example.m4359touchscreen.R;
 import com.example.m4359touchscreen.Helpers.SectionsPageAdapter;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
 
-    private TabLayout mReadingsTabs;
-    private TabLayout mConfigTabs;
-    private SectionsPageAdapter mReadingsSectionsPageAdapter;
-    private SectionsPageAdapter mConfigSectionsPageAdapter;
-    private ViewPager mReadingsViewPager;
-    private ViewPager mConfigViewPager;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mReadingsTabs = (TabLayout)findViewById(R.id.ReadingsTabLayout);
-        mConfigTabs = (TabLayout)findViewById(R.id.ConfigTabLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mReadingsViewPager = (ViewPager)findViewById(R.id.ReadingsViewPager);
-        mConfigViewPager = (ViewPager)findViewById(R.id.ConfigViewPager);
+        drawer = findViewById(R.id.layout_drawer);
 
-        List<Fragment> readingsFragments = new ArrayList<>();
-        List<String> readingsTitles = new ArrayList<>();
-        readingsFragments.add(new ExtensometerReadingsFragment());
-        readingsFragments.add(new TemperatureReadingFragment());
-        readingsFragments.add(new LoadReadingsFragment());
-        readingsFragments.add(new TimerReadingsFragment());
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        readingsTitles.add("Extens");
-        readingsTitles.add("Temps");
-        readingsTitles.add("Load");
-        readingsTitles.add("Timers");
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        List<Fragment> configFragments = new ArrayList<>();
-        List<String> configTitles = new ArrayList<>();
-        configFragments.add(new StatusConfigFragment());
-        configFragments.add(new ParametersConfigFragment());
-        configFragments.add(new OptionsConfigFragment());
-        configFragments.add(new ControlsConfigFragment());
+    }
 
-        configTitles.add("Status");
-        configTitles.add("Params");
-        configTitles.add("Options");
-        configTitles.add("Control");
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_control:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ControlsFragment()).commit();
+//                Toast.makeText(this, "Control", Toast.LENGTH_SHORT).show();
+                break;
 
-        List<List<Fragment>> listOfFragmentsLists = new ArrayList<>(Arrays.asList(readingsFragments, configFragments));
-        List<List<String>> listOfTitlesLists = new ArrayList<>(Arrays.asList(readingsTitles, configTitles));
-        List<ViewPager> listOfViewPagers = new ArrayList<>(Arrays.asList(mReadingsViewPager, mConfigViewPager));
+            case R.id.nav_sensor_setup:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new ChatFragment()).commit();
+                Toast.makeText(this, "Sensor Setup", Toast.LENGTH_SHORT).show();
+                break;
 
-        SectionsPageAdapter.setupViewPager(getSupportFragmentManager(), listOfViewPagers, listOfFragmentsLists, listOfTitlesLists);
+            case R.id.nav_sensor_calibration:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new ProfileFragment()).commit();
+                Toast.makeText(this, "Sensor Calibration", Toast.LENGTH_SHORT).show();
+                break;
 
-        mReadingsTabs.setupWithViewPager(mReadingsViewPager);
-        mConfigTabs.setupWithViewPager(mConfigViewPager);
+            case R.id.nav_test_settings:
+                Toast.makeText(this, "Test Settings", Toast.LENGTH_SHORT).show();
+                break;
 
+            case R.id.nav_graphing:
+                Toast.makeText(this, "Graphing", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
